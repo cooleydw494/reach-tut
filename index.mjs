aac150b8bda1c44a0b55cc32cf58f731e7f60dd6
cooleydw494 <cooleydw494@gmail.com>
2022-03-16 04:22:26 -0500

    back-end changes for timeouts

--- index.mjs
+++ index.mjs
@@ -26,6 +26,9 @@ const Player = (Who) => ({
     seeOutcome: (outcome) => {
         console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
     },
+    informTimeout: () => {
+        console.log(`${Who} observed a timeout`);
+    },
 });
 
 await Promise.all([
